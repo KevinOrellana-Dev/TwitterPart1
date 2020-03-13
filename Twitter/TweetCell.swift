ea43d33045117f59d3d531ec2be0276953538e61
admin <admin@USCS-Mac243.local>
2020-03-12 22:00:48 -0700

    Part 2 commit 1

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class TweetCell: UITableViewCell {
     
+    @IBOutlet weak var retweetButton: UIButton!
     
+    @IBOutlet weak var favButton: UIButton!
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
@@ -26,5 +28,20 @@ class TweetCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    @IBAction func favoriteTweet(_ sender: Any) {
+        
+        var favorited: Bool = false
+        func setFavorited(_isFavorited:Bool)
+        {
+            favorited = _isFavorited
+            
+            if(favorited)
+            {
+                
+            }
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
 }
